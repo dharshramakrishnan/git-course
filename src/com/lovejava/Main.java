d25612d76f97c917c445b1dfb4272b0241a2d0ec
dharsh <priyaramakrishnan1629@gmail.com>
2025-07-29 19:20:35 +0530

    New version release

--- src/com/lovejava/Main.java
+++ src/com/lovejava/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args)
     {
     	System.out.println("First code push to git");
-    	
+    	System.out.println("Adding tag....");
     }
 }
